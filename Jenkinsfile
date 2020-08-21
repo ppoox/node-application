fdb14e61dd2319e86358f42cadf55462f73e93fd
ppoox <ppoox@naver.com>
2020-08-21 09:52:31 +0900

    edit Jenkinsfile && move .sh

--- Jenkinsfile
+++ Jenkinsfile
@@ -15,7 +15,7 @@ pipeline {
 		sh '''
 		    TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 		    scp -o StrictHostKeyChecking=no -r application1 koh@10.0.2.15:~/바탕화면/node-application/application$TIMESTAMP
-		    ssh koh@10.0.2.15 ./application.sh $TIMESTAMP
+		    ssh koh@10.0.2.15 'cd /home/koh/바탕화면/node-application; ./application.sh $TIMESTAMP'
 		'''
 	    }
 	}
---
+++ application.sh
@@ -0,0 +1,44 @@
+# move application directory
+cd /home/koh/바탕화면/node-application/application$1
+npm install
+
+# getting avaliable port
+a=$(lsof -Pi:3001 -sTCP:LISTEN -t)
+b=$(lsof -Pi:3002 -sTCP:LISTEN -t)
+after=0
+before=0
+
+if((${a} > 0))
+then
+    echo '3001 running, 3002 start'
+    after=3002
+    before=3001
+elif((${b} > 0))
+then
+    echo '3002 running, 3001 start'
+    after=3001
+    before=3002
+else
+    echo 'not match, 3001 start'
+    after=3001
+fi
+
+# after version start
+if((${after} > 0)) 
+then
+    echo 'after start'
+    PORT=${after} pm2 start server.js --name ${after}
+fi
+
+sleep 10
+
+if((${before} > 0))
+then 
+    echo 'before stop'
+    pm2 stop ${before}
+    pm2 delete ${before}
+fi
+
+cp -RT /home/koh/바탕화면/node-application/application$1 /home/koh/바탕화면/node-application/application1
+
+
